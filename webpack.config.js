a644a093dad3166cda0b0e31b70afb9162397fd3
Evgeniy <va.evgenii@gmail.com>
2020-09-15 14:29:19 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,124 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const cssLoaders = (extra) => {
+  const loaders = [{
+    loader: MiniCssExtractPlugin.loader,
+    options: {
+      hmr: isDev,
+      reloadAll: true,
+    },
+  }, 'css-loader'];
+  if (extra) {
+    loaders.push(extra)
+  }
+
+  return loaders
+}
+
+console.log('IS DEV', isDev);
+console.log('IS PROD', isProd);
+
+const optimization = () => {
+  const config = {};
+
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+    ]
+  };
+
+  return config;
+};
+
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: ['@babel/polyfill', './index.js'],
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'bundle'),
+  },
+  optimization: optimization(),
+  devtool: isDev ? 'source-map' : '',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/favicon.ico'),
+          to: path.resolve(__dirname, 'bundle')
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders(),
+      },
+      {
+        test: /\.less$/,
+        use: cssLoaders('less-loader'),
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader'),
+      },
+      {
+        test: /\.(png|svg|ttf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          }
+        }
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    hot: isDev,
+  }
+}
